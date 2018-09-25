df5cf796fa0481b44ba17bef497591c0ef32a796
tomclindstrom <tom.c.lindstrom@gmail.com>
2018-09-26 00:49:15 +0300

    Create battre.js
    test

---
+++ js/battre.js
@@ -0,0 +1,55 @@
+var button = document.getElementById("generate");
+
+        button.addEventListener("click", function () {
+
+            //get random numbers
+
+            var numbers = [];
+            var ball = document.getElementsByClassName("ball");
+
+            while (numbers.length < 7) {
+
+                var random = Math.floor(Math.random() * 39) + 1;
+
+                if (numbers.indexOf(random) == -1) {
+                    numbers.push(random);
+                }
+            }
+
+            //sort numbers in array
+
+            numbers.sort(function (a, b) {
+                return a - b;
+            });
+
+            //color balls
+
+            for (var i = 0; i < ball.length; i++) {
+
+                ball[i].style.backgroundColor = "white";
+                ball[i].style.color = "#3d3c3a";
+
+                for (var j = 0; j < numbers.length; j++) {
+                    if (numbers[j] == parseInt(ball[i].innerHTML)) {
+                        ball[i].style.backgroundColor = "#fa8b60";
+                        ball[i].style.color = "white";
+                    }
+                }
+            }
+
+            //add numbers to history
+
+            var history = [];
+            var historyDiv = document.getElementById("history");
+            var para = document.createElement("p");
+            history.push(numbers.join(", "));
+
+            for (var k in history) {
+
+                var node = document.createTextNode(history[k]);
+                para.appendChild(node);
+                historyDiv.appendChild(para);
+
+            }
+
+        });
